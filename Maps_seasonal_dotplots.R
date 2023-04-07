a388a71113d04468166308eeb6c42041f14d9ecf
acrunyon <amber.c.runyon@gmail.com>
2023-04-07 08:14:31 -0600

    Add mapping plots

---
+++ Maps_seasonal_dotplots.R
@@ -0,0 +1,123 @@
+var = "precipIn"
+long.title = "total precipitation (in/season)" 
+delta.var = "pcp"
+scale="viridis"
+
+shp = wrst # area is wrst
+
+# create CF file lists
+rds.ls = list.files(path = dir, pattern = paste0(var,"_"), full.names = TRUE)
+CF1.ls = Filter(function(x) grepl(paste(GCMs[1], collapse = "|"), x), rds.ls)
+CF2.ls = Filter(function(x) grepl(paste(GCMs[2], collapse = "|"), x), rds.ls)
+CF3.ls = Filter(function(x) grepl(paste(GCMs[3], collapse = "|"), x), rds.ls)
+
+# read in RDS for setting scale limits
+# CF1
+DJF.cf1 <- readRDS(Filter(function(x) grepl(paste("DJF", collapse = "|"), x), CF1.ls))
+MAM.cf1 <- readRDS(Filter(function(x) grepl(paste("MAM", collapse = "|"), x), CF1.ls))
+JJA.cf1 <- readRDS(Filter(function(x) grepl(paste("JJA", collapse = "|"), x), CF1.ls))
+SON.cf1 <- readRDS(Filter(function(x) grepl(paste("SON", collapse = "|"), x), CF1.ls))
+# CF2
+DJF.cf2 <- readRDS(Filter(function(x) grepl(paste("DJF", collapse = "|"), x), CF2.ls))
+MAM.cf2 <- readRDS(Filter(function(x) grepl(paste("MAM", collapse = "|"), x), CF2.ls))
+JJA.cf2 <- readRDS(Filter(function(x) grepl(paste("JJA", collapse = "|"), x), CF2.ls))
+SON.cf2 <- readRDS(Filter(function(x) grepl(paste("SON", collapse = "|"), x), CF2.ls))
+# CF3
+DJF.cf3 <- readRDS(Filter(function(x) grepl(paste("DJF", collapse = "|"), x), CF3.ls))
+MAM.cf3 <- readRDS(Filter(function(x) grepl(paste("MAM", collapse = "|"), x), CF3.ls))
+JJA.cf3 <- readRDS(Filter(function(x) grepl(paste("JJA", collapse = "|"), x), CF3.ls))
+SON.cf3 <- readRDS(Filter(function(x) grepl(paste("SON", collapse = "|"), x), CF3.ls))
+
+scale.min = min(c(DJF.cf1$pcp_in,MAM.cf1$pcp_in,JJA.cf1$pcp_in,SON.cf1$pcp_in,
+                  DJF.cf2$pcp_in,MAM.cf2$pcp_in,JJA.cf2$pcp_in,SON.cf2$pcp_in,
+                  DJF.cf3$pcp_in,MAM.cf3$pcp_in,JJA.cf3$pcp_in,SON.cf2$pcp_in),na.rm=TRUE)
+scale.max = max(c(DJF.cf1$pcp_in,MAM.cf1$pcp_in,JJA.cf1$pcp_in,SON.cf1$pcp_in,
+                  DJF.cf2$pcp_in,MAM.cf2$pcp_in,JJA.cf2$pcp_in,SON.cf2$pcp_in,
+                  DJF.cf3$pcp_in,MAM.cf3$pcp_in,JJA.cf3$pcp_in,SON.cf2$pcp_in),na.rm=TRUE)
+
+# ggplot
+map.plot <- function(data, title,xaxis,metric,col){
+  ggplot() + 
+    geom_raster(data = ak_df ,aes(x = x, y = y,alpha=HYP_HR_SR_W.1), show.legend=FALSE) +
+    geom_stars(data = data, alpha = 0.8) + 
+    geom_sf(data = shp, aes(), fill = NA) + 
+    scale_fill_viridis(direction=-1, option = scale, 
+                       limits = c(scale.min, scale.max), oob = scales::squish) + 
+    labs(title = title) +
+    theme_map() +
+    theme(legend.position = "bottom",
+          legend.key.width = unit(6, "cm"),
+          legend.key.height = unit(.3, "cm"),
+          legend.justification = "center",
+          # plot.title=element_blank(),
+          plot.title=element_text(size=12,face="bold",hjust=0.5),
+          plot.background = element_rect(colour = col, fill=NA, size=5),
+          plot.margin = unit(c(.5,0,0,0), "cm")) + 
+    labs(fill = metric)
+}
+
+# CF1
+DJF.cf1.plot <- map.plot(data=DJF.cf1, title=CFs[1],metric=paste0("Average ",long.title),col=CF_GCM$CF_col[1])
+MAM.cf1.plot <- map.plot(data=MAM.cf1, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[1])
+JJA.cf1.plot <- map.plot(data=JJA.cf1, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[1])
+SON.cf1.plot <- map.plot(data=SON.cf1, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[1])
+
+# CF2
+DJF.cf2.plot <- map.plot(data=DJF.cf2, title=CFs[2],metric=paste0("Average ",long.title),col=CF_GCM$CF_col[2])
+MAM.cf2.plot <- map.plot(data=MAM.cf2, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[2])
+JJA.cf2.plot <- map.plot(data=JJA.cf2, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[2])
+SON.cf2.plot <- map.plot(data=SON.cf2, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[2])
+
+# CF3
+DJF.cf3.plot <- map.plot(data=JJA.cf3, title=CFs[3],metric=paste0("Average ",long.title),col=CF_GCM$CF_col[3])
+MAM.cf3.plot <- map.plot(data=MAM.cf3, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[3])
+JJA.cf3.plot <- map.plot(data=JJA.cf3, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[3])
+SON.cf3.plot <- map.plot(data=SON.cf3, title="",metric=paste0("Average ",long.title),col=CF_GCM$CF_col[3])
+
+###########################################################
+##### NONE OF THESE WORK ##################################
+# Merge into one plot
+
+maps.all <- grid_arrange_shared_legend(DJF.cf1.plot,DJF.cf2.plot,DJF.cf3.plot, # THIS WORKS, BUT WANT TO HAVE SHARED RC TITLES
+                                       MAM.cf1.plot,MAM.cf2.plot,MAM.cf3.plot,    # FOR CF AND SEASON
+                                       JJA.cf1.plot,JJA.cf2.plot,JJA.cf3.plot,
+                                       SON.cf1.plot,SON.cf2.plot,SON.cf3.plot,
+                                       nrow = 4,ncol=3, position = "bottom") 
+
+djf <- textGrob("DJF", gp = gpar(fontface="bold", size = 12))
+mam <- textGrob("MAM", gp = gpar(fontface="bold", size = 12))
+jja <- textGrob("JJA", gp = gpar(fontface="bold", size = 12))
+son <- textGrob("SON", gp = gpar(fontface="bold", size = 12))
+
+seasons <- grid.arrange(djf,mam,jja,son,ncol=1)
+
+maps <- grid.arrange(seasons,maps.all,ncol = 2, widths = c(1,15))
+
+
+################################### MONTHLY DOT PLOT ##################
+
+
+dotplot <- ggplot(delta, aes(x=(eval(parse(text=delta.var))),y=season,fill=CF)) +
+  geom_vline(xintercept=0, linetype="dashed", color = "black") + 
+  geom_point(stat="identity",size=8,colour="black",aes(fill = factor(CF), shape = factor(CF))) +
+  theme(axis.text=element_text(size=16),    #Text size for axis tick mark labels
+        axis.title.x=element_blank(),               #Text size and alignment for x-axis label
+        plot.title=element_blank(),
+        # axis.title.y=element_text(size=16, vjust=0.5,  margin=margin(t=20, r=20, b=20, l=20)),              #Text size and alignment for y-axis label
+        # plot.title=element_text(size=20, vjust=0.5, face="bold", margin=margin(t=20, r=20, b=20, l=20)),      #Text size and alignment for plot title
+        legend.title=element_text(size=16),                                                                    #Text size of legend category labels
+        legend.text=element_text(size=14),                                                                   #Text size of legend title
+        legend.position = "bottom")  +
+  labs(title = paste0("Change in seasonal ",long.title), 
+       x = "Change (°F)", y = "") +
+  scale_fill_manual(name="",values =cols) +
+  scale_shape_manual(name="",values = c(21,22,23)) +
+  scale_y_discrete(limits=rev)
+dotplot
+
+g <- grid.arrange(maps, dotplot,ncol = 2, widths = c(6, 4), clip = FALSE)
+
+annotate_figure(g, top = text_grob(paste0("Change in seasonal ",long.title, "; 1950-1999 vs 2025-2055"), 
+                                   face = "bold", size = 20))
+
+ggsave(paste0("seasonal_",var,".png"), width = 15, height = 9, path = plot.dir)
---
+++ Maps_ts_plots_ANNTmean.R
@@ -0,0 +1,117 @@
+library(tidyr)
+library(dplyr)
+library(stars)
+library(raster)
+library(here)
+library(ggplot2)
+library(ggthemes)
+library(zoo)
+library(viridis)
+library(ggbreak)
+library(lemon)
+library(ggpubr);library(gridExtra);library(grid);library(gtable)
+
+rm(list=ls())
+
+data.dir <- "C:/Users/achildress/Documents/Git-repos/HI_Climate_Data/data/Output/Data-files/"
+plot.dir <- "C:/Users/achildress/Documents/Git-repos/HI_Climate_Data/data/Output/Plots/"
+
+#ANNPrecip -- load .rds files
+CF1.rds <- readRDS(paste0(data.dir,"ANNTmeanDelta_rcp45"))  #change var name
+CF2.rds <- readRDS(paste0(data.dir,"ANNTmeanDelta_rcp85"))  #change var name
+  
+boundary <-boundary <- st_read('./data/HALE/HALE_boundary.shp')
+boundary <- st_transform(boundary, st_crs(CF1.rds))
+CF_GCM <- data.frame(CF=c("Climate Future 1", "Climate Future 2"), scen=c("rcp45","rcp85"))
+cols <- c("#9A9EE5","#E10720")
+
+var = "TmeanF" #change to name of var in df
+long.title = "average annual temperature (\u00B0F)" #change to be legend
+delta.var = "TmeanF"
+scale="inferno"
+
+# Generate sample data for ts plot
+df = read.csv(paste0(data.dir,"Tmean.monthly.HALE.csv")) 
+df = merge(df, CF_GCM,by="scen",all=TRUE)
+df$CF[which(is.na((df$CF)))] = "Recent"
+# df$CF_col[which(is.na((df$CF_col)))] = "grey"
+df$CF = factor(df$CF, levels=c("Recent",CF_GCM$CF))
+df$Year = as.Date(format(as.Date(df$date,format="%Y-%m-%d"),"%Y"),format="%Y")
+DF=aggregate(TmeanF~Year+CF,df,mean)
+DF$period = factor(ifelse(DF$Year<"2010-01-01","Past","Future"),levels=c("Past","Future"))
+  
+  means <- DF %>% group_by(CF) %>%
+    summarize(var = mean(eval(parse(text=var)))) 
+  
+  scale.min = min(c(CF1.rds$TmeanF, CF2.rds$TmeanF),na.rm=TRUE)
+  scale.max = max(c(CF1.rds$TmeanF, CF2.rds$TmeanF),na.rm=TRUE)
+  
+  # ggplot
+  map.plot <- function(data, title,xaxis,metric,col){
+    ggplot() + 
+      geom_stars(data = data, alpha = 0.8) + 
+      geom_sf(data = boundary, aes(), fill = NA) +
+      scale_fill_viridis(direction=-1, option = scale, limits = c(scale.min, scale.max),  
+                         guide = guide_colorbar(title.position = "top", title.hjust = 0.5),oob = scales::squish) + #mako for WB delta
+      labs(title = title) +
+      theme_map() +
+      theme(legend.position = "bottom",
+            legend.key.width = unit(6, "cm"),
+            legend.key.height = unit(.3, "cm"),
+            legend.justification = "center",
+            plot.title=element_text(size=12,face="bold",hjust=0.5),
+            plot.background = element_rect(colour = col, fill=NA, linewidth = 5)) + 
+      labs(fill = metric)
+  }
+  cf1 <- map.plot(data=CF1.rds,title="Climate Future 1",metric=long.title,col=cols[1])
+  cf2 <- map.plot(data=CF2.rds,title="Climate Future 2",metric=long.title,col=cols[2])
+  
+ ts<-ggplot(DF, aes(x=Year, y=(eval(parse(text=var))), group=CF, colour = CF)) +
+    geom_line(colour = "black",size=2.5, stat = "identity") +
+    geom_line(size = 2, stat = "identity") +
+    geom_point(colour= "black", size=4, aes(fill = factor(CF), shape = factor(CF))) +
+    theme(axis.text=element_text(size=14),
+          # axis.text.x=element_blank(),
+          axis.title.x=element_text(size=16,vjust=1.0),
+          axis.title.y=element_text(size=16,vjust=1.0),
+          plot.title=element_blank(),
+          legend.text=element_text(size=14), legend.title=element_text(size=14),
+          legend.position = "bottom") +
+    labs(title = paste0("Change in annual ",long.title), 
+         x = "Year", y = long.title) +
+    scale_color_manual(name="",values = c("grey",cols)) +
+    scale_fill_manual(name="",values = c("grey",cols)) +
+    scale_shape_manual(name="",values = c(21,22,23)) +
+    facet_wrap(~period, nrow = 1, ncol=3,scales = "free_x") 
+    # coord_fixed(ratio = .3) 
+  ts
+  
+  
+  #### Just maps and ts plot
+  maps <- grid_arrange_shared_legend(cf1, cf2,ncol = 2, nrow = 1, position = "bottom", 
+                                     top = textGrob(paste0("Change in ",long.title),
+                                                    gp=gpar(fontface="bold", col="black", fontsize=16)))
+  # g <- ggarrange(maps,ts, nrow=2)
+  # g
+  
+  #### Maps, ts, table
+  delta.var <- means
+  delta.var$var[2:3] <- delta.var$var[2:3] - delta.var$var[1]
+  delta.var$var <- signif(delta.var$var, digits = 1)
+  
+  table <- tableGrob(delta.var, rows = NULL,cols=NULL)
+  # table <- gtable_add_grob(table, grobs = rectGrob(gp = gpar(fill=NA, lwd=2)), #library(gtable)
+  #                          t=4,b=nrow(table),l=1,r=ncol(table))
+  table <- annotate_figure(table,
+                           top = text_grob("Recent = absolute value \n CFs = change values", color = "black",
+                                           face = "italic", size = 12))
+  tsplots <- grid.arrange(ts, table,ncol = 2, widths = c(4, 1), clip = FALSE)
+  
+  
+  g <- ggarrange(maps,tsplots, nrow=2)+bgcolor("white") 
+  g
+  
+
+  ggsave(paste0(var,"_ANN.png"), width = 15, height = 9, path = plot.dir)
+  
+  
